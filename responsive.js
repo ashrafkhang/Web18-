8ba3ffc181553cc3a053dbeb362cf729028ad48e
ashrafkhang <81286858+ashrafkhang@users.noreply.github.com>
2021-11-06 14:08:16 +0530

    Add files via upload

---
+++ responsive.js
@@ -0,0 +1,11 @@
+
+// navigation bar responsible
+function myFunction() {
+  var x = document.getElementById("myTopnav");
+  if (x.className === "topnav") {
+    x.className += " responsive";
+  } else {
+    x.className = "topnav";
+  }
+}
+
